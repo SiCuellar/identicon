65faf213b541b8e79ea846b3cc17daf172a82961
Silvestre Cuellar <cuellarsilvestre@gmail.com>
2019-07-23 16:33:09 -0600

    used Chunk enum to split map

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -3,6 +3,13 @@ defmodule Identicon do
     input
     |> hash_input
     |> pick_color
+    |> build_grid
+  end
+
+  def build_grid(%Identicon.Image{hex: hex} = image) do
+    hex
+    |>
+    Enum.chunk(3)
   end
 
   #pattern match to get access to first 3 properties
@@ -17,7 +24,7 @@ defmodule Identicon do
   # end
   # we can directy patter match in teh fucntion input
 
-  
+
 
 
 
