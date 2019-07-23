7e84dd345a056b16e3e9d7f48a0e7cd7ac112a82
Silvestre Cuellar <cuellarsilvestre@gmail.com>
2019-07-22 23:48:42 -0600

    creating structs

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -1,11 +1,11 @@
 defmodule Identicon do
   def main(input) do
     input
-    |> hash_input(input)
+    |> hash_input
   end
 
   def hash_input(input) do
     :crypto.hash(:md5, input)
-    |> binary().bin_to_list 
+    |> binary.bin_to_list
   end
 end
